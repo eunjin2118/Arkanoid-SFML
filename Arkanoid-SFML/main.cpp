c67a06007b31668c8f0ae6ed2a4a772868a3145b
eunjin2118 <ally6324@naver.com>
2022-05-24 12:20:01 +0900

    화면띄우기

--- Arkanoid-SFML/main.cpp
+++ Arkanoid-SFML/main.cpp
@@ -15,5 +15,33 @@ void main(void)
 	t3.loadFromFile("imgaes/bail.png");
 	t4.loadFromFile("imgaes/paddle.png");
 
+	Sprite sBackground(t2), sBall(t3), sPaddle(t4);
+	sPaddle.setPosition(300, 400);
+	sBall.setPosition(300, 300);
 
+	Sprite block[1000];
+	int n = 0;
+	for (int x = 1; x <= 10; x++) {
+		for (int y = 0; y <= 10; y++) {
+			block[n].setTexture(t1);
+			block[n].setPosition(x * 43, y * 20);
+			n++;
+		}
+	}
+
+	while (app.isOpen()) {
+		Event e;
+		while (app.pollEvent(e)) {
+			if (e.type == Event::Closed)
+				app.close();
+		}
+		app.clear();
+		app.draw(sBackground);
+		app.draw(sBall);
+		app.draw(sPaddle);
+		for (int i = 0; i < n; i++)
+			app.draw(block[i]);
+
+		app.display();
+	}
 }
