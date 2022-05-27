1544e22f1f3af289a4421f1ff80afa0693c7d539
eunjin2118 <ally6324@naver.com>
2022-05-27 10:24:26 +0900

    화면 바깥 테두리에 대한 공의 충돌처리

--- Arkanoid-SFML/main.cpp
+++ Arkanoid-SFML/main.cpp
@@ -6,9 +6,8 @@ void main(void)
 {
 	// window창 생성
 	RenderWindow app(VideoMode(520, 450), "BB");
-	// 60프레임 1초에 60개의 그림이 있는걸로 제한
 	app.setFramerateLimit(60);
-	
+
 	Texture t1, t2, t3, t4;
 	t1.loadFromFile("images/block01.png");
 	t2.loadFromFile("images/background.jpg");
@@ -21,26 +20,39 @@ void main(void)
 
 	Sprite block[200];
 	int n = 0;
-	for (int x = 1; x <= 10; x++) {
-		for (int y = 0; y <= 10; y++) {
+	for (int x = 1; x <= 10; x++)
+	{
+		for (int y = 0; y <= 10; y++)
+		{
 			block[n].setTexture(t1);
 			block[n].setPosition(x * 43, y * 20);
 			n++;
 		}
 	}
-	
-	// 숫자 -> 이동하는 속도조절 + 각도조절
-	float dx = 6.0f, dy = 5.0f;
 
-	while (app.isOpen()) {
+	// 숫자 -> 이동하는 속도조절 + 각도조절
+	float dx = 5.0f, dy = 6.0f;
+
+	while (app.isOpen())
+	{
 		Event e;
-		while (app.pollEvent(e)) {
+		while (app.pollEvent(e))
+		{
 			if (e.type == Event::Closed)
 				app.close();
 		}
 		// 공 움직이기
 		sBall.move(dx, 0);
 		sBall.move(0, dy);
+
+		// 볼의 위치(좌표)
+		Vector2f b = sBall.getPosition();
+
+		// 화면 바깥쪽 테두리에 충돌하는 경우
+		if (b.x < 0 || b.x>520)
+			dx = -dx;
+		if (b.y < 0 || b.y > 450)
+			dy = -dy;
 
 		app.clear();
 		app.draw(sBackground);
@@ -51,4 +63,5 @@ void main(void)
 
 		app.display();
 	}
+
 }
